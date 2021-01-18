8f7a8d5fee2e1a05dfd628bf56142a9ff6ef83cc
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2021-01-18 10:11:47 -0800

    epic: server setup; docker compose

--- scripts/build/local.sh
+++ scripts/build/local.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-sudo docker system prune && sudo docker build -t mern-blackbelt-exam:"$1" --target stage "."
+sudo docker system prune && sudo docker build -t mern-blackbelt-exam:"$1" "."
